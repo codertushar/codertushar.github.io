6f30fce0fabe2ed0829879fa7e1a3753cd894efd
Tushar Khanna <tushar.khanna@catalystone.com>
2020-05-09 15:59:37 +0530

    Updates

---
+++ precache-manifest.bf1d18aa93d57d972349c746bd0247cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d456d12c77d3448dad4e20dbf3e287ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "151e2c977f0621341459",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65f2ba135db55689a5f3",
+    "url": "/static/js/2.dfb4f2de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfb4f2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151e2c977f0621341459",
+    "url": "/static/js/main.624d2c01.chunk.js"
+  },
+  {
+    "revision": "1cb1b3b54146a054d030",
+    "url": "/static/js/runtime-main.e062d820.js"
+  },
+  {
+    "revision": "38ab05f81037d6e1f10c8fbfaa6c6245",
+    "url": "/static/media/profile.38ab05f8.jpg"
+  }
+]);
